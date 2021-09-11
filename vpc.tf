17da4b17178cfafb6a23a22b946b10366e5def9d
cd72 <chris.j.denney@gmail.com>
2021-09-11 19:03:29 +0100

    fixed bugs and fmt

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,10 @@
 resource "aws_vpc" "main" {
-    cidr_block = var.vpc_cidr
-    instance_tenancy = "default"
-    enable_dns_hostnames = "true"
-    enable_dns_support = "true"
+  cidr_block           = var.vpc_cidr
+  instance_tenancy     = "default"
+  enable_dns_hostnames = "true"
+  enable_dns_support   = "true"
 
-    tags = var.tags
+  tags = local.tags
 
 
 
@@ -12,11 +12,11 @@ resource "aws_vpc" "main" {
 
 
 resource "aws_subnet" "public" {
-    count = length(var.public_subnets) 
+  count = length(var.pub_subnets)
 
-    vpc_id = aws_vpc.this[0].vpc_id
-    cidr_block = element(concat(var.pub_subnets, [""]), count.index)
-    availability_zone = "${element(data.aws_availability_zones.names, count.index)}"
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = element(concat(var.pub_subnets, [""]), count.index)
+  availability_zone = element(data.aws_availability_zones.availability_zones.names, count.index)
 
-    tags = var.tags
+  tags = local.tags
 }
