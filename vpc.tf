2b605fbc6f4818ba25fb0c4a543ed75bd9f4c0d2
cd72 <chris.j.denney@gmail.com>
2021-09-12 13:21:24 +0100

    bug fixes

--- vpc.tf
+++ vpc.tf
@@ -20,3 +20,31 @@ resource "aws_subnet" "public" {
 
   tags = local.tags
 }
+
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress = [
+    {
+      description      = "SSH from VPC"
+      from_port        = 22
+      to_port          = 22
+      protocol         = "tcp"
+      cidr_blocks      = [aws_vpc.main.cidr_block]
+    }
+  ]
+
+  egress = [
+    {
+      from_port        = 0
+      to_port          = 0
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  ]
+
+  tags = var.tags
+}
