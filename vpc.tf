b01c2e55aeabe0ccb15c00413336c5592e9930d2
cd72 <chris.j.denney@gmail.com>
2021-09-12 14:55:09 +0100

    bug fixes

--- vpc.tf
+++ vpc.tf
@@ -61,3 +61,21 @@ resource "aws_internet_gateway" "gw" {
 
   tags = local.tags
 }
+
+
+resource "aws_route_table" "main_route_table" {
+  count = length(var.pub_subnets)
+
+  vpc_id = aws_vpc.main.id
+
+  tags = local.tags
+}
+
+resource "aws_route" "internet_gateway" {
+  count = length(var.pub_subnets)
+
+  route_table_id            = aws_route_table.main_route_table.id
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = aws_internet_gateway.gw.id
+         
+}
