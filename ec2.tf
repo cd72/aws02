60a300173ce5526ee7f9d47426bb9348d5204a1f
cd72 <chris.j.denney@gmail.com>
2021-09-12 21:05:27 +0100

    fix bug to add id

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "bastion_server" {
   ami           = "ami-0194c3e07668a7e36"
   instance_type = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.allow_ssh]
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   tags = merge(
       local.tags,
